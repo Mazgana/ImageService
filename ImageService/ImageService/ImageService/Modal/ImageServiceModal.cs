1b50811b1433f9216a58f73c2fd9f30075251bd0
mazgana <adi.mazgani@gmail.com>
2018-03-29 21:42:11 +0300

    add file method

--- ImageService/ImageService/ImageService/Modal/ImageServiceModal.cs
+++ ImageService/ImageService/ImageService/Modal/ImageServiceModal.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Globalization;
 
 namespace ImageService.Modal
 {
@@ -20,31 +22,34 @@ namespace ImageService.Modal
             String response = "Image copied successfully.";
             try
             {
-                FileInfo imageInfo = new FileInfo(path);
-                DateTime date = imageInfo.CreationTime;
-                String year = date.Year.ToString();
-                String month = date.Month.ToString();
+                DateTime date = GetDateTakenFromImage(path);
+                string year = date.Year.ToString();
+                string month = date.Month.ToString();
 
-                string newFile = Path.GetFileName(path);
+                Console.WriteLine(year);
+
+                string filename = Path.GetFileName(path);
                 String dest = outputDir + @"\" + year + @"\" + month;
                 response = CreateFolder(dest, out result);
                 if (result == false)
                     return response;
 
-                File.Copy(path, dest + @"\" + newFile);
+                File.Copy(path, dest + @"\" + filename);
 
                 dest = outputDir + @"\Thumbnail\" + year + @"\" + month;
                 response = CreateFolder(dest, out result);
                 if (result == false)
                     return response;
 
-                File.Copy(path, dest + @"\" + "thumb" + newFile);
+                string thumbPath = dest + @"\" + "thumb-" + filename;
 
                 Image image = Image.FromFile(path);
                 int size;
                 Int32.TryParse(ConfigurationManager.AppSettings["thumbnailSize"], out size);
                 Image thumb = image.GetThumbnailImage(size, size, () => false, IntPtr.Zero);
+                FileInfo imageInfo = new FileInfo(thumbPath);
                 thumb.Save(Path.ChangeExtension(imageInfo.FullName, "thumb"));
+                //Path.ChangeExtension(imageInfo.FullName, null);
 
             } catch (Exception e)
             {
@@ -54,6 +59,30 @@ namespace ImageService.Modal
 
             result = true;
             return response;
+        }
+
+        //retrieves the datetime WITHOUT loading the whole image
+        public static DateTime GetDateTakenFromImage(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (Image myImage = Image.FromStream(fs, false, false))
+            {
+                // Get the Date Created property 
+                PropertyItem propertyItem = myImage.PropertyItems.FirstOrDefault(i => i.Id == 306);
+                if (propertyItem != null)
+                {
+                    // Extract the property value as a String. 
+                    ASCIIEncoding encoding = new ASCIIEncoding();
+                    string text = encoding.GetString(propertyItem.Value, 0, propertyItem.Len - 1);
+
+                    // Parse the date and time. 
+                    CultureInfo provider = CultureInfo.InvariantCulture;
+                    return DateTime.ParseExact(text, "yyyy:MM:d H:m:s", provider);
+                } else
+                {
+                    return DateTime.Now;
+                }
+            }
         }
 
         public string CreateFolder(string dest, out bool result)
