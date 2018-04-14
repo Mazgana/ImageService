1467df7cc106436225c26182f1840cad24e7c3fa
wachtfl <leah.wach@gmail.com>
2018-04-14 20:36:49 +0300

    some more comments

--- ImageService/ImageService/ImageService/Controller/Handlers/DirectoyHandler.cs
+++ ImageService/ImageService/ImageService/Controller/Handlers/DirectoyHandler.cs
@@ -14,11 +14,14 @@ using ImageService.Modal.Event;
 
 namespace ImageService.Controller.Handlers
 {
+    /*
+     * this class handles a specific directory, monitoring changes and executing it's commands.
+     */ 
     public class DirectoyHandler : IDirectoryHandler
     {
         #region Members
         private IImageController m_controller;              // The Image Processing Controller
-        private ILoggingService m_logging;
+        private ILoggingService m_logging;                  // The logger
         private FileSystemWatcher m_dirWatcher;             // The Watcher of the Dir
         private string m_path;                              // The Path of directory
         DateTime lastRead = DateTime.MinValue;
@@ -27,6 +30,7 @@ namespace ImageService.Controller.Handlers
         // The Event That Notifies that the Directory is being closed
         public event EventHandler<DirectoryCloseEventArgs> DirectoryClose;
 
+        //constructor. saves members and creates file watcher
         public DirectoyHandler(String path, IImageController controller, ILoggingService logging)
         {
             this.m_path = path;
@@ -35,7 +39,8 @@ namespace ImageService.Controller.Handlers
             m_dirWatcher = new FileSystemWatcher();
         }
 
-        // The Function Recieves the directory to Handle
+        // The Function Recieves the directory to Handle,
+        //directory watcher watches specified directory for changes.
         public bool StartHandleDirectory(string dirPath)
         {
             if (System.IO.Directory.Exists(dirPath))
@@ -52,10 +57,11 @@ namespace ImageService.Controller.Handlers
                 return false;   //the directory isn't exists
             }
         }
-        
+
+        // this function is called on when directory changed. if change is a new file' sends a command.
         private void OnChanged(object sender, FileSystemEventArgs e)
         {
-                DateTime lastWriteTime = File.GetLastWriteTime(e.FullPath);
+                DateTime lastWriteTime = File.GetLastWriteTime(e.FullPath);//checks if the change is a new file added.
                 if (lastWriteTime != this.lastRead && isImage(e.FullPath))
                 {
                     this.lastRead = lastWriteTime;
@@ -65,6 +71,7 @@ namespace ImageService.Controller.Handlers
                 }
         }
 
+        //function checks if given file path is an image
         private bool isImage(string path)
         {
             string ext = new FileInfo(path).Extension;
@@ -74,7 +81,7 @@ namespace ImageService.Controller.Handlers
             return false;
         }
 
-        // The Event that will be activated upon new Command
+        // The Event that will be activated upon new Command, executing the specified command.
         public void OnCommandRecieved(object sender, CommandRecievedEventArgs e)
         {
             m_logging.Log("recieved command for directory: " + e.RequestDirPath, MessageTypeEnum.INFO);
@@ -86,12 +93,13 @@ namespace ImageService.Controller.Handlers
             }
         }
 
+        // function will be activated upon when server is closing, closing the handler.
         public void onClose(object send, DirectoryCloseEventArgs e)
         {
             m_logging.Log("closing handler for directory: " + m_path, MessageTypeEnum.INFO);
-            m_dirWatcher.EnableRaisingEvents = false;
+            m_dirWatcher.EnableRaisingEvents = false;//stopping the directory watcher
             m_dirWatcher.Dispose();
-            DirectoryClose?.Invoke(this, e);
+            DirectoryClose?.Invoke(this, e);//calling on server close function, to be removed from event list
         }
     }
 }
