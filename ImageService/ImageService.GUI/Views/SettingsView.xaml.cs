c6312f908b6dee0860debfea2b7069d09663e6d0
mazgana <adi.mazgani@gmail.com>
2018-05-01 21:52:11 +0300

    Adding to tabs to the main window

--- ImageService/ImageService.GUI/Views/SettingsView.xaml.cs
+++ ImageService/ImageService.GUI/Views/SettingsView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ImageService.GUI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace ImageService.GUI.Views
         public SettingsView()
         {
             InitializeComponent();
+            this.DataContext = new SettingsViewModel();
         }
     }
 }
