33b43300f549978e9ff4fd07bfb29d6f2d2bb4c8
wachtfl <leah.wach@gmail.com>
2018-05-17 16:42:39 +0300

    closehandler erases from config.exe

--- ImageService/leah/Program.cs
+++ ImageService/leah/Program.cs
@@ -14,7 +14,7 @@ namespace leah
             Console.WriteLine("main\n");
             TcpClientChannel client = new TcpClientChannel();
             Console.WriteLine("created tcp client");
-            client.SendCommand(new ImageService.Communication.Model.CommandMessage(2, "just trying"));
+            client.SendCommand(new ImageService.Communication.Model.CommandMessage(4, "C:\\Users\\as\\Videos"));
             Console.WriteLine("sent message");
             client.RecieveCommand();
             Console.WriteLine("end. got message");
