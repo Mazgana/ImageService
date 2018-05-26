90265174b6166c2ebc8250b111b994b34c74e54a
mazgana <adi.mazgani@gmail.com>
2018-05-27 02:49:13 +0300

    design

---
+++ ImageService/ImageService.GUI/Converters/ConnectionToColorConverter.cs
@@ -0,0 +1,31 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ImageService.GUI.Converters
+{
+    class ConnectionToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(Brush))
+                throw new InvalidOperationException("Must convert to a brush!");
+
+            bool conneted = (bool)value;
+            if (conneted)
+                return Brushes.LightBlue;
+            else
+                return Brushes.LightGray;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
