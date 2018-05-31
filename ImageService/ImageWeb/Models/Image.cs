c1608f96d16399c1e8a797ec9e202048b7c4accf
mazgana <adi.mazgani@gmail.com>
2018-05-31 21:05:03 +0300

    Adding views for all pages and methods to the home controller

---
+++ ImageService/ImageWeb/Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImageWeb.Models
+{
+    public class Image
+    {
+        string Nmae { get; set; }
+        DateTime date { get; set; }
+        string path { get; set; }
+        string thumbPath { get; set; }
+    }
+}
