91668d6fa46d6135a522c741e0ab70fa343e1ba0
mazgana <adi.mazgani@gmail.com>
2018-06-04 22:46:03 +0300

    adding  modles

---
+++ ImageService/WebApplication2/WebApplication2/Models/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Config
+    {
+        string outputDirectory { get; set; }
+        string sourceName { get; set; }
+        string logName { get; set; }
+        string thumbSize { get; set; }
+        List<string> handlers { get; set; }
+    }
+}
