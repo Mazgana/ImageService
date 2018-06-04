de7b1e4d7fc05ae0cfb54b40c12772ed5b87b9a7
wachtfl <leah.wach@gmail.com>
2018-06-04 08:05:52 +0300

    from working together on  starting web

---
+++ ImageService/ImageWeb/Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImageWeb.Models
+{
+    public class Image
+    {
+        public string name { get; set; }
+        public DateTime date { get; set; }
+        public string path { get; set; }
+        public string thumbPath { get; set; }
+    }
+}
